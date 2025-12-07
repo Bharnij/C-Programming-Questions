7f91738f41c55c5c8adb12352272a362526ac4cb
Bharnij <bharnijborah108@gmail.com>
2025-12-07 21:18:16 +0530

    Uploaded questions alongwith solutions

---
+++ To find the greatest number out of 3 numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1, num2, num3;
+
+    printf("Enter your first nunmber ");
+    scanf("%d", &num1);
+
+    printf("Enter your second nunmber ");
+    scanf("%d", &num2);
+
+    printf("Enter your third nunmber ");
+    scanf("%d", &num3);
+
+    if (num1>num2){
+        if (num1>num3)
+            printf("%d is the greatest ", num1);
+        else
+            printf("%d Is the greatest ", num3);
+
+    }
+
+    else{
+        if (num2>num3)
+            printf("%d is the greatest ", num2);
+
+        else
+            printf("%d is the greatest ", num3);
+    }
+
+    return 0;
+}
---
+++ To reverse a number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+int n, digits, rev_digits;
+
+printf("Enter the number - ");
+scanf("%d", &n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
