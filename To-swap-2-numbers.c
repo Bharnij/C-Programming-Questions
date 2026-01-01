14ba67d31c257de644478a2c5bddcf6895962639
Bharnij <bharnijborah108@gmail.com>
2026-01-01 19:14:47 +0530

    Uploaded new question

---
+++ To-swap-2-numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+int a,b, temp;
+
+printf("Enter the values of the numbers\n -");
+scanf("%d %d", &a, &b);
+
+temp = a;
+a = b;
+b = temp;
+
+printf("The values of the numbers after swapping are - " "%d %d ", a , b);
+
+return 0;
+
+}
