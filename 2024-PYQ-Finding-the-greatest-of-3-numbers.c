f18cbb3956e791fda940ab438be3eeede008e2cd
Bharnij <bharnijborah108@gmail.com>
2026-01-05 20:14:38 +0530

    modified code

--- 2024-PYQ-Finding-the-greatest-of-3-numbers.c
+++ 2024-PYQ-Finding-the-greatest-of-3-numbers.c
@@ -21,15 +21,11 @@ int main() {
     printf("Enter your third nunmber ");
     scanf("%d", &num3);
 
-    if (num1>num2){
-        if (num1>num3)
+    if ((num1>num2 && num1>num3)) {
             printf("%d is the greatest ", num1);
-        else
-            printf("%d Is the greatest ", num3);
-
     }
 
-    else{
+    else {
         if (num2>num3)
             printf("%d is the greatest ", num2);
 
