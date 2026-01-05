33d5adc4f9d821fb2d515fd66821ff2a034b1201
Bharnij <bharnijborah108@gmail.com>
2026-01-05 23:04:32 +0530

    Modified comments

--- 2024-PYQ.c
+++ 2024-PYQ.c
@@ -15,27 +15,23 @@ int main(){
 
     // Part 2 - creating arrays for names and marks
 
-    char names[n][30]; // i don't think anybody's name is above 30 characters, 
-                                    // hence we are alotting a max of 29 characters per name
-                                    // feel free to add as much character limit as you want
-    
-    int marks[n]; // this is an integer array - stores marks of each student
+    char names[n][30]; 
+
+    int marks[n];
     
     // Part 3 - taking student inputs
 
-    for (int i=0; i<n; i++){ // this loop runs n times from i=0 to i=n-1
-                             // i++ is used to increase the value of i by 1, after every time the loop runs once
+    for (int i=0; i<n; i++){ 
     
     printf("Name of the student %d - ", i+1);
-    scanf("%s", names[i]); // %s is a format specifier just like %d - it reads a word with no spaces
-                           // names[i] is a 2D array where rows = students and columns = characters
-                           // i=0 stores 1st student name , i=1 stores 2nd student name .....
+    scanf("%s", names[i]); 
 
     // Part 4 - taking student marks
 
     printf("Enter marks %d", i+1);
-    scanf("%d", &marks[i]); // marks is stored in the marks array at index i
-
+    scanf("%d", &marks[i]); 
+    
+    
     // Part 5 - Finding the highest or max marks
 
     int max = marks[0];
